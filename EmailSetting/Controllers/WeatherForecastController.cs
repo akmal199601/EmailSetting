5c8b4fdb7c889a59b5ff1c081baa64623c86c8ef
akmal199601 <62462485+akmal199601@users.noreply.github.com>
2022-06-02 16:40:31 +0500

    Добавьте файлы проекта.

---
+++ EmailSetting/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+﻿using EmailSetting.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailSetting.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		private readonly ILogger<WeatherForecastController> _logger;
+
+		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			var rng = new Random();
+			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = DateTime.Now.AddDays(index),
+				TemperatureC = rng.Next(-20, 55),
+				Summary = Summaries[rng.Next(Summaries.Length)]
+			})
+			.ToArray();
+		}
+		[HttpGet("EmailMessage")]
+		public async Task<IActionResult> SendMessage()
+		{
+			EmailServices emailService = new EmailServices();
+			await emailService.SendEmailAsync("somemail@mail.ru", "Тема письма", "Тест письма: тест!");
+			return RedirectToAction("Index");
+		}
+	}
+}
---
+++ EmailSetting/Services/EmailServices.cs
@@ -0,0 +1,28 @@
+﻿using MimeKit;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+
+namespace EmailSetting.Services
+{
+	public class EmailServices
+	{
+		public async Task SendEmailAsync(string email,string subject,string message)
+		{
+			var emailMessage = new MimeMessage();
+
+			emailMessage.From.Add(new MailboxAddress("Admin", "akmal199601@mail.ru"));
+			emailMessage.To.Add(new MailboxAddress("",email));
+			emailMessage.Subject = subject;
+			emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+			{
+				Text = message
+			};
+			using var client = new SmtpClient();
+			await client.ConnectAsync("smtp.yandex.ru", 25, false);
+			await client.AuthenticateAsync("akmal199601@mail.ru", "password");
+			await client.SendAsync(emailMessage);
+
+			await client.DisconnectAsync(true);
+		}
+	}
+}
